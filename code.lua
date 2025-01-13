80ec15258465e43302633ce2e9e4e8e886744567
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-01-13 11:32:49 +0100

    update code.lua

--- code.lua
+++ code.lua
@@ -1,28 +1,29 @@
-
--- List of registed snippets.
+-- List of registered snippets
 local snippet_names = {
-  "snippet1", 
-  "snippet2", 
-  "snippet3"
-  
+    "doas",
+    "invman",
+    "afk_peter"
 }
 
+-- Funktion, um alle Snippets auszuführen
 local function run_all_snippets()
     for _, snippet_name in ipairs(snippet_names) do
-        minetest.run_snippet("", snippet_name)
+        snippets.run(snippet_name)
     end
 end
 
+-- Chat-Befehl registrieren, um alle Snippets auszuführen
 minetest.register_chatcommand("run_snippets", {
-    description = "run all snippets",
+    description = "Führe alle Snippets aus",
     privs = {server = true},
     func = function(name)
         run_all_snippets()
-        return true, "start of all snippets was successfully."
+        return true, "Start aller Snippets war erfolgreich."
     end,
 })
 
+-- Führe den Befehl 'run_snippets' aus
 minetest.after(0, function()
-    minetest.chat_send_all("starting snippets ...")
+    minetest.chat_send_all("Starte Snippets...")
     run_all_snippets()
 end)
